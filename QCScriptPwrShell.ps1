a4ca06b98509a3726623a8553006d3bdb073f1c9
Christopher <christopher.hall@embl.it>
2020-08-20 14:03:39 +0200

    Add files via upload

---
+++ QCScriptPwrShell.ps1
@@ -0,0 +1,4 @@
+"Automated QC program.  Window will close automatically when finshed"
+"For details contact: Christopher Hall, christopher.hall@embl.it"
+
+& "C:\Program Files\R\R-4.0.2\bin\Rscript.exe" "Q:\User_QC\QCscriptv21_Attune.R"
